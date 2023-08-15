cc774fbd062e067b5251508fc224dadbad259d19
meeplabsdev <76091175+meeplabsdev@users.noreply.github.com>
2023-08-15 12:41:00 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,154 +1,152 @@
 from pydeezer import Deezer
-from pydeezer.constants import track_formats
-from http.server import BaseHTTPRequestHandler, HTTPServer
 
-import socket
+from serverui import MusicPlayerApp
+from serverhoster import Hoster
+
 import json
 import time
 import threading
+import datetime
 
 with open("config.json") as json_data_file:
     config = json.load(json_data_file)
 
-start_time  = time.time()
-
-HOST = "127.0.0.1" 
-PORT = 36671 # TCP, HTTP is (PORT + 1)
-NAME = "Ethan's Music"
-PLAYING = False
-QUEUE = [
-    {"name": "THX", "artist": "CG5", "id": "", "length": 0}, 
-    {"name": "Lonely King", "artist": "CG5", "id": "", "length": 0}, 
-    {"name": "Noot Noot", "artist": "CG5", "id": "", "length": 0}
-]
-SONG = QUEUE[0]
-
-playbackSeconds = 0
-
 ARL = config["arl"]
 deezer = Deezer(arl=ARL)
 
-def nextSong():
-    global start_time
-    global playbackSeconds
-    global SONG
-    global QUEUE
-
-    sin = 0
-    tin = 0
-    for song in QUEUE:
-        if song == SONG:
-            tin = sin + 1
-
-            if tin > len(QUEUE) - 1:
-                tin = 0
-
-            SONG = QUEUE[tin]
-            playbackSeconds = 0
-            start_time = time.time() + 5
-
-            return True
-        sin += 1
-    return False
-
-def hasSongEnded(song, playbackSeconds):
-    return int(song["length"]) + 5 <= playbackSeconds
-
-def getSongTime():
-    if (PLAYING):
-        return playbackSeconds
-    else:
-        return -2147483647
-
-def getSongInfo():
-    if (PLAYING):
-        if (SONG["id"] == ""):
-            SONG["id"] = deezer.search_tracks(f"{SONG['name']} {SONG['artist']}")[0]["id"]
-        if (SONG["length"] == 0):
-            track = deezer.get_track(SONG["id"])
-            SONG["length"] = track["info"]["DATA"]["DURATION"]
-        return {"name": SONG["name"], "artist": SONG["artist"], "id": SONG["id"], "length": SONG["length"], "time": getSongTime()}
-    else:
-        return {"name": "  ", "artist": "  ", "time": getSongTime()}
-
-def respond(id):
-    match id:
-        case "reg":
-            response_data = {"success": True, "name": NAME}
-        case "song":
-            response_data = {"success": True, "song": getSongInfo()}
-        case _:
-            response_data = {"success": False}
-
-
-    response_json = json.dumps(response_data)
-    print(f"Responded: {response_json}")
-    return response_json.encode("utf-8")
-
-def respondReq():
-    while True:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((HOST, PORT))
-            s.listen()
-            conn, addr = s.accept()
-            with conn:
-                print(f"Connected by {addr}", end="")
-                while True:
-                    data = conn.recv(1024)
-                    if not data:
-                        break
-                    print(f" - Recieved '{data.decode('utf-8')}'")
-                    conn.sendall(respond(data.decode("utf-8")))
-
-r_thread = threading.Thread(target=respondReq)
-r_thread.daemon = True
-r_thread.start()
-
-class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        self.wfile.write(respond((self.path.replace("\\", "/").replace("/", ""))))
-
-def respondReqHttp():
-    webServer = HTTPServer(("localhost", PORT + 1), MyServer)
-
+def timeToSeconds(time_str):
     try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
+        # Parse the time string into a timedelta object
+        time_delta = datetime.datetime.strptime(time_str, "%H:%M:%S") - datetime.datetime(1900, 1, 1)
+        # Convert timedelta to total seconds
+        total_seconds = time_delta.total_seconds()
+        return total_seconds
+    except ValueError:
+        raise ValueError("Invalid time format. Use HH:MM:SS")
+
+class Player:
+    def __init__(self, name, ui):
+        self.name = name
+        self.queue = []
+        self.song = None
+        self.playing = False
+
+        self.start_time = time.time()
+        self.playback_seconds = 0
+
+        self.app = ui
+
+        ui.overrideme_play = self.play
+        ui.overrideme_stop = self.stop
+        ui.overrideme_next = self.next
+
+    def play(self):        
+        self.start_time = time.time() + 5
+        self.playback_seconds = 0
+
+        self.playing = True
         pass
 
-    webServer.server_close()
+    def stop(self):
+        self.playing = False
+        pass
 
-rh_thread = threading.Thread(target=respondReqHttp)
-rh_thread.daemon = True
-rh_thread.start()
+    def next(self):
+        self.next_song()
+        pass
 
-def incrementPlaybackSeconds():
-    global playbackSeconds
-    global start_time
+    def next_song(self):
+        sin = 0
+        tin = 0
+        for song in self.queue:
+            if song == self.song:
+                tin = sin + 1
 
-    while True:
-        playbackSeconds = time.time() - start_time
+                if tin > len(self.queue) - 1:
+                    tin = 0
 
-p_thread = threading.Thread(target=incrementPlaybackSeconds)
-p_thread.daemon = True
-p_thread.start()
+                self.song = self.queue[tin]
+                self.playback_seconds = 0
+                self.start_time = time.time() + 5
 
-for T_SONG in QUEUE:
-    if (SONG["id"] == ""):
-            SONG["id"] = deezer.search_tracks(f"{SONG['name']} {SONG['artist']}")[0]["id"]
-    if (SONG["length"] == 0):
-        track = deezer.get_track(SONG["id"])
-        SONG["length"] = track["info"]["DATA"]["DURATION"]
+                return True
+            sin += 1
+        return False
 
+    def has_song_ended(self):
+        return int(self.song["length"]) + 5 <= self.playback_seconds
 
-print("Ready!")
-start_time  = time.time()
-PLAYING = True
+    def get_song_time(self):
+        if (self.playing):
+            return self.playback_seconds
+        else:
+            return 0
 
-while True:
-    if (PLAYING):
-        if (hasSongEnded(SONG, playbackSeconds)):
-            nextSong()
+    def get_song_info(self):
+        if (self.playing):
+            return {"name": self.song["name"], "artist": self.song["artist"], "id": self.song["id"], "length": self.song["length"], "time": self.get_song_time()}
+        else:
+            return {"name": "  ", "artist": "  ", "id": 0, "length": 0, "time": self.get_song_time()}
+
+    def respond(self, id):
+        self.update_current_playing()
+
+        match id:
+            case "reg":
+                response_data = {"success": True, "name": self.name}
+            case "song":
+                response_data = {"success": True, "song": self.get_song_info()}
+            case _:
+                response_data = {"success": False}
+
+        response_json = json.dumps(response_data)
+        print(f"Responded: {response_json}")
+        return response_json.encode("utf-8")
+    
+    def get_queue(self):
+        queue = []
+        viewqueue = self.app.get_queue()
+        for viewitem in viewqueue:
+            name = viewitem[0]
+            artist = viewitem[1]
+            id = viewitem[2]
+            duration = timeToSeconds(viewitem[3])
+
+            queue.append({"name": name, "artist": artist, "id": id, "length": duration})
+        return queue
+    
+    def update_current_playing(self):
+        self.queue = self.get_queue()
+        if (self.playing):
+            self.song = self.queue[0]
+
+    def increment_playback_seconds(self):
+        self.playback_seconds = time.time() - self.start_time
+
+    def mainloop(self):
+        while True:
+            self.app.playing = self.playing
+            self.queue = self.get_queue()
+
+            if (self.playing and self.song == None):
+                if (len(self.queue) > 0):
+                    self.song = self.queue[0]
+                else:
+                    self.playing = False
+
+            self.increment_playback_seconds()
+            if (self.playing):
+                self.app.update_slider(int(self.playback_seconds), self.song["length"])
+                if (self.has_song_ended()):
+                    self.app.next_song() # Simulate button press
+
+if __name__ == "__main__":
+    app = MusicPlayerApp()
+    controller = Player("Ethan's Music", app)
+    hoster = Hoster(controller.respond)
+
+    mainloop = threading.Thread(target=controller.mainloop)
+    mainloop.daemon = True
+    mainloop.start()
+
+    app.mainloop()
